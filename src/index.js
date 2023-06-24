3f9341054b44bc7add02334360fe85e9dfeb8ddb
Pinya2477 <pinya.su.65@ubu.ac.th>
2023-06-24 12:00:19 +0700

    proflie card

--- src/index.js
+++ src/index.js
@@ -1,8 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles.css";
+
+
+function toThaiDateString(date) {
+  let monthNames = [
+    "มกราคม",
+    "กุมภาพันธ์",
+    "มีนาคม",
+    "เมษายน",
+    "พฤษภาคม",
+    "มิถุนายน",
+    "กรกฎาคม",
+    "สิงหาคม.",
+    "กันยายน",
+    "ตุลาคม",
+    "พฤศจิกายน",
+    "ธันวาคม",
+  ];
+
+  let year = date.getFullYear() + 543;
+  let month = monthNames[date.getMonth()];
+  let numOfDay = date.getDate();
+
+  let hour = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let second = date.getSeconds().toString().padStart(2, "0");
+
+  return `${numOfDay} ${month} ${year} ` + `${hour}:${minutes}:${second} น.`;
+}
+
+let date1 = new Date();
+console.log(toThaiDateString(date1));
+
+
+function MyDateTime(){
+  let date1 = new Date();
+  date1 = toThaiDateString(date1);
+  return <div>{date1}</div>;
+}
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Unity" emoji="😁 " color="cyan" />
+      <Skill skill="PHP" emoji="😃" color="orange" />
+      <Skill skill="React" emoji="😅" color="yellow" />
+      <Skill skill="photoshop" emoji="😊" color="orangered" />
+      <Skill skill="Mysql" emoji="😂" color="Pink" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+
+
+function Intro(){
+  return(
+    <div>
+      <h1>ภิญญา  สุขวิพัฒน์</h1>
+
+      <p>
+        นักศึกษาสาขาวิชาเทคโนโลยีสารสนเทศและนวัตกรรมดิจิทัล คณะวิทยาศาสตร์  มหาวิทยาลัยอุบลราชธานี
+        ชื่อชอบการเล่นกีฬา เทคโนโลยีสมัยใหม่ ...
+      </p>
+
+
+    </div>
+  )
+}
+
+function Avatar(){
+  return <img className="avatar" src="Pinya.jpg" alt="Pinya Sukwiphat"></img>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +102,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
